41e9c9205d8fbd48eea447f1cc0b24c8d42fb8d2
Tom <tjpalmer@tjpalmer.com>
2020-08-20 08:09:30 -0700

    Reorder haskell lets

--- Infer.hs
+++ Infer.hs
@@ -8,6 +8,6 @@ enumerate = zip [(0 :: Int)..]
 -- main :: IO ()
 main = do
     let colors = ["red", "yellow", "blue"]
-        showColor = uncurry $ printf "Color %d: %s"
         indexedColors = enumerate colors  -- :: [(Int, String)]
+        showColor = uncurry $ printf "Color %d: %s"
     mapM_ putStrLn $ map showColor indexedColors
